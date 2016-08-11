88cd76a570b57800c29a3471ccf92f623cd02405
ryan.chen <ryan.chen@harman.com>
2016-08-11 17:06:05 +0800

    add composite pattern

---
+++ app/src/main/java/com/example/ryan/hkgankio/designpattern/composite/JpgFile.java
@@ -0,0 +1,23 @@
+package com.example.ryan.hkgankio.designpattern.composite;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by studio02 on 8/11/16.
+ */
+public class JpgFile implements Dir{
+    @Override
+    public void print() {
+        Logger.d("TEST_DESIGN_PATTERN --->我是JPG文件");
+    }
+
+    @Override
+    public void addFile(Dir dir) {
+        throw new NoSuchFieldError();
+    }
+
+    @Override
+    public void removeFile(Dir dir) {
+        throw new NoSuchFieldError();
+    }
+}
