570c4d9f2430f79affc7d9bcc1f874b4b9af6f19
ryanchen <ryan.chen@harman.com>
2016-08-12 23:51:49 +0800

    add design pattern flyweight

---
+++ app/src/main/java/com/example/ryan/hkgankio/designpattern/Flyweight/Device.java
@@ -0,0 +1,15 @@
+package com.example.ryan.hkgankio.designpattern.Flyweight;
+
+/**
+ * Created by ryan on 8/12/16.
+ */
+public class Device {
+
+    private String name;
+    private long uuid;
+
+    public Device(String name, long uuid) {
+        this.name = name;
+        this.uuid = uuid;
+    }
+}
