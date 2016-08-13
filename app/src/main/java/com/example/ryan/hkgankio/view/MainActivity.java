d342d70b5a8d3390ae900624cb542ef2794a488e
ryanchen <ryan.chen@harman.com>
2016-08-13 13:44:20 +0800

    add clone design pattern

--- app/src/main/java/com/example/ryan/hkgankio/view/MainActivity.java
+++ app/src/main/java/com/example/ryan/hkgankio/view/MainActivity.java
@@ -3,12 +3,11 @@ package com.example.ryan.hkgankio.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,20 +15,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.ryan.hkgankio.R;
 import com.example.ryan.hkgankio.common.HKCommon;
-<<<<<<< HEAD
-import com.example.ryan.hkgankio.designpattern.bridge.ActivityBridgeResponsibity;
-=======
-import com.example.ryan.hkgankio.designpattern.composite.ActivityCompositePattern;
->>>>>>> develop
-import com.example.ryan.hkgankio.designpattern.decorator.ActivityDecoratorPattern;
-import com.example.ryan.hkgankio.designpattern.facade.ActivityFacadePattern;
-import com.example.ryan.hkgankio.designpattern.mediator.ActivityMeditorPattern;
-import com.example.ryan.hkgankio.designpattern.observer.ActivityObserverPattern;
-import com.example.ryan.hkgankio.designpattern.proxy.ActivityProxyPattern;
-import com.example.ryan.hkgankio.designpattern.template.ActivityTemplatePattern;
 import com.example.ryan.hkgankio.designpattern.visitor.ActivityVisitorPattern;
 import com.example.ryan.hkgankio.util.ToolBarControler;
 import com.example.ryan.hkgankio.util.ToolBarInfo;
