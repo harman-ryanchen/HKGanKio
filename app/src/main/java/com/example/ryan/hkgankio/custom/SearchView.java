16b43286857b09f49e713d1c8f5ca017338dac18
Ryan <ryan.chen@harman.com>
2016-12-05 18:02:59 +0800

    search View

---
+++ app/src/main/java/com/example/ryan/hkgankio/custom/SearchView.java
@@ -0,0 +1,162 @@
+package com.example.ryan.hkgankio.custom;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.ryan.hkgankio.R;
+import com.orhanobut.logger.Logger;
+
+
+/**
+ * Created by studio02 on 11/24/16.
+ */
+
+public class SearchView extends View {
+    private Paint mPaint = new Paint();
+    private int viewWidth,viewHeight;
+    private int defaultDuring = 2000;
+    private float animatorValue = 0;
+    private ValueAnimator searchAnimator;
+    private ValueAnimator.AnimatorUpdateListener animatorUpdateListener;
+    Path path_search;
+    Path path_circle;
+    private PathMeasure mPathMeasure;
+
+    public SearchView(Context context) {
+        this(context,null);
+    }
+
+    public SearchView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public SearchView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initAll();
+
+    }
+
+    private void initAll() {
+        initPaint();
+        initPath();
+        initListner();
+        initValueAnimation();
+
+        searchAnimator.start();
+    }
+
+    private void initListner() {
+        animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                animatorValue = (float) animation.getAnimatedValue();
+                invalidate();
+            }
+        };
+    }
+
+    private void initValueAnimation() {
+        searchAnimator = ValueAnimator.ofFloat(0,1).setDuration(defaultDuring);
+        searchAnimator.addUpdateListener(animatorUpdateListener);
+    }
+
+
+    private void initPath() {
+         path_search = new Path();
+         path_circle = new Path();
+
+        mPathMeasure  = new PathMeasure();
+//        RectF searchRect = new RectF(-50,-50,50,50);
+//        path_search.addArc(searchRect,45,359.9f);
+
+        RectF circleRect = new RectF(-100,-100,100,100);
+        path_search.addArc(circleRect,45,-359.9f);
+
+//        float pos[] = new float[2];
+//        mPathMeasure.setPath(path_circle,false);
+//        mPathMeasure.getPosTan(0,pos,null);
+//
+//        path_search.lineTo(pos[0],pos[1]);
+
+//        Logger.d("TEST_CUSTOM_VIEW pos[0] = %s , pos[1] = %s",pos[0],pos[1]);
+    }
+
+    private void initPaint() {
+        mPaint.setColor(R.color.white);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10);
+    }
+
+    /**
+     * measure size of the view
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthsize = MeasureSpec.getSize(widthMeasureSpec);      //取出宽度的确切数值
+        int widthmode = MeasureSpec.getMode(widthMeasureSpec);      //取出宽度的测量模式
+
+        int heightsize = MeasureSpec.getSize(heightMeasureSpec);    //取出高度的确切数值
+        int heightmode = MeasureSpec.getMode(heightMeasureSpec);    //取出高度的测量模式
+
+        Logger.d("TEST_Custom_View widthsize = %s , widthmode = %s , heightsize = %s , heightmode = %s", widthsize, widthmode, heightsize, heightmode);
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * if you want change or confirm the view`s with and height
+     *
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //confirm the view`s  with and height
+//        setMeasuredDimension(w,h);
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawSearchView(canvas);
+    }
+
+    private void drawSearchView(Canvas canvas) {
+        canvas.translate(viewWidth/2,viewHeight/2);
+
+//        canvas.drawPath(path_circle,mPaint);
+//        canvas.drawPath(path_search,mPaint);
+        //获取circle轨迹，以及截取
+        mPaint.setColor(R.color.blue);
+        Path dst = new Path();
+        mPathMeasure.setPath(path_circle,false);
+        float stop = mPathMeasure.getLength() * animatorValue;
+        float start = (float) (stop - 0.3);
+        Logger.d("TEST_CUSTOM_VIEW path measure lenghth =%s , animatior value = %s stop = %s start = %s",mPathMeasure.getLength() ,animatorValue,stop,start);
+        mPathMeasure.getSegment(start,stop,dst,true);
+        canvas.drawPath(dst,mPaint);
+
+    }
+
+}
