c53034a1d309087d36cb7f5a33a18f9d56da1db2
Ryan <ryan.chen@harman.com>
2016-12-06 15:38:13 +0800

    finish search View

--- app/src/main/java/com/example/ryan/hkgankio/custom/SearchView.java
+++ app/src/main/java/com/example/ryan/hkgankio/custom/SearchView.java
@@ -1,5 +1,6 @@
 package com.example.ryan.hkgankio.custom;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -8,6 +9,8 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,21 +24,25 @@ import com.orhanobut.logger.Logger;
 
 public class SearchView extends View {
     private Paint mPaint = new Paint();
-    private int viewWidth,viewHeight;
+    private int viewWidth, viewHeight;
     private int defaultDuring = 2000;
     private float animatorValue = 0;
     private ValueAnimator searchAnimator;
+    private ValueAnimator startAnimator;
+    private ValueAnimator endAnimator;
     private ValueAnimator.AnimatorUpdateListener animatorUpdateListener;
+    private ValueAnimator.AnimatorListener mAnimatorListener;
     Path path_search;
     Path path_circle;
     private PathMeasure mPathMeasure;
+    private SearchState currentState = SearchState.NONE;
 
     public SearchView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public SearchView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public SearchView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -44,14 +51,48 @@ public class SearchView extends View {
 
     }
 
+    private enum SearchState {
+        NONE,
+        STARING,
+        SEARCHING,
+        END;
+    }
+
     private void initAll() {
         initPaint();
         initPath();
         initListner();
         initValueAnimation();
 
-        searchAnimator.start();
+        currentState = SearchState.STARING;
+        startAnimator.start();
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (currentState) {
+                case NONE:
+                    currentState = SearchState.STARING;
+                    startAnimator.start();
+                    break;
+                case STARING:
+                    currentState = SearchState.SEARCHING;
+                    searchAnimator.start();
+                    break;
+                case SEARCHING:
+                    currentState = SearchState.END;
+                    endAnimator.start();
+                    break;
+                case END:
+                    currentState = SearchState.NONE;
+                    handler.sendEmptyMessageDelayed(0,2000);
+                    break;
+
+            }
+        }
+    };
 
     private void initListner() {
         animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
@@ -61,32 +102,64 @@ public class SearchView extends View {
                 invalidate();
             }
         };
+        mAnimatorListener = new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                handler.sendEmptyMessage(0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        };
     }
 
     private void initValueAnimation() {
-        searchAnimator = ValueAnimator.ofFloat(0,1).setDuration(defaultDuring);
+        searchAnimator = ValueAnimator.ofFloat(0, 1).setDuration(defaultDuring);
+        startAnimator = ValueAnimator.ofFloat(0, 1).setDuration(defaultDuring);
+        endAnimator = ValueAnimator.ofFloat(1, 0).setDuration(defaultDuring);
+
+        searchAnimator.addListener(mAnimatorListener);
+        startAnimator.addListener(mAnimatorListener);
+        endAnimator.addListener(mAnimatorListener);
+
+
         searchAnimator.addUpdateListener(animatorUpdateListener);
+        startAnimator.addUpdateListener(animatorUpdateListener);
+        endAnimator.addUpdateListener(animatorUpdateListener);
     }
 
 
     private void initPath() {
-         path_search = new Path();
-         path_circle = new Path();
+        path_search = new Path();
+        path_circle = new Path();
 
-        mPathMeasure  = new PathMeasure();
-//        RectF searchRect = new RectF(-50,-50,50,50);
-//        path_search.addArc(searchRect,45,359.9f);
+        mPathMeasure = new PathMeasure();
+        RectF searchRect = new RectF(-50, -50, 50, 50);
+        path_search.addArc(searchRect, 45, 359.9f);
 
-        RectF circleRect = new RectF(-100,-100,100,100);
-        path_search.addArc(circleRect,45,-359.9f);
+        RectF circleRect = new RectF(-100, -100, 100, 100);
+        path_circle.addArc(circleRect, 45, -359.9f);
 
-//        float pos[] = new float[2];
-//        mPathMeasure.setPath(path_circle,false);
-//        mPathMeasure.getPosTan(0,pos,null);
-//
-//        path_search.lineTo(pos[0],pos[1]);
 
-//        Logger.d("TEST_CUSTOM_VIEW pos[0] = %s , pos[1] = %s",pos[0],pos[1]);
+        float pos[] = new float[2];
+        mPathMeasure.setPath(path_circle, false);
+        mPathMeasure.getPosTan(0, pos, null);
+
+        path_search.lineTo(pos[0], pos[1]);
+
+        Logger.d("TEST_CUSTOM_VIEW pos[0] = %s , pos[1] = %s", pos[0], pos[1]);
     }
 
     private void initPaint() {
@@ -124,7 +197,6 @@ public class SearchView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         //confirm the view`s  with and height
-//        setMeasuredDimension(w,h);
         super.onSizeChanged(w, h, oldw, oldh);
         viewWidth = w;
         viewHeight = h;
@@ -143,19 +215,51 @@ public class SearchView extends View {
     }
 
     private void drawSearchView(Canvas canvas) {
-        canvas.translate(viewWidth/2,viewHeight/2);
+
+
+        canvas.drawColor(Color.parseColor("#0082D7"));
+
+        canvas.translate(viewWidth / 2, viewHeight / 2);
+
+        switch (currentState) {
+            case NONE:
+                canvas.drawPath(path_search, mPaint);
+                break;
+            case STARING:
+                mPathMeasure.setPath(path_search, false);
+                Path dst = new Path();
+                mPathMeasure.getSegment(mPathMeasure.getLength() * animatorValue, mPathMeasure.getLength(), dst, true);
+                canvas.drawPath(dst, mPaint);
+                break;
+            case SEARCHING:
+                mPathMeasure.setPath(path_circle, false);
+                Path dct = new Path();
+                float stop = mPathMeasure.getLength() * animatorValue;
+                float start = (float) (stop - ((0.5 - Math.abs(animatorValue - 0.5)) * 200f));
+                mPathMeasure.getSegment(start, stop, dct, true);
+                canvas.drawPath(dct, mPaint);
+                break;
+            case END:
+                mPathMeasure.setPath(path_search, false);
+                Path dst1 = new Path();
+                mPathMeasure.getSegment(mPathMeasure.getLength() * animatorValue, mPathMeasure.getLength(), dst1, true);
+                canvas.drawPath(dst1, mPaint);
+                break;
+        }
+
 
 //        canvas.drawPath(path_circle,mPaint);
 //        canvas.drawPath(path_search,mPaint);
         //获取circle轨迹，以及截取
-        mPaint.setColor(R.color.blue);
+//        canvas.drawPath(path_circle,mPaint);
         Path dst = new Path();
-        mPathMeasure.setPath(path_circle,false);
-        float stop = mPathMeasure.getLength() * animatorValue;
-        float start = (float) (stop - 0.3);
-        Logger.d("TEST_CUSTOM_VIEW path measure lenghth =%s , animatior value = %s stop = %s start = %s",mPathMeasure.getLength() ,animatorValue,stop,start);
-        mPathMeasure.getSegment(start,stop,dst,true);
-        canvas.drawPath(dst,mPaint);
+        mPathMeasure.setPath(path_circle, false);
+//        float stop = mPathMeasure.getLength() * animatorValue;
+//        float start = (float) (stop - 10);
+//        Logger.d("TEST_CUSTOM_VIEW path measure lenghth =%s , animatior value = %s stop = %s start = %s",mPathMeasure.getLength() ,animatorValue,stop,start);
+//        mPathMeasure.getSegment(start,stop,dst,true);
+//        mPaint.setColor(Color.WHITE);
+//        canvas.drawPath(dst,mPaint);
 
     }
 
